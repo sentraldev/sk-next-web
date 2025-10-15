f27eb957f594e548643b6e6f3578de987b5f3d40
James Christian Wira <jamesch888@gmail.com>
2025-10-15 14:54:29 +0700

    fix: update code for location page

--- src/app/our-location/page.tsx
+++ src/app/our-location/page.tsx
@@ -2,15 +2,15 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import LocationStoreUI from "./components/LocationStoreUI";
 
-export default function OutLocationPage() {
+export default function OurLocationPage() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen bg-gray-50">
       <Header />
-      <div className="content-width mx-auto py-8">
+      <div className="content-width mx-auto w-full py-8">
         <h1 className="text-2xl font-bold mb-4">Lokasi Kami</h1>
         <LocationStoreUI />
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
